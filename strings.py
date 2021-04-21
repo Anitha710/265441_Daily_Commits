aa5d73f72ca699f79d84a7c831fbbff25b9ba0a4
Anitha B <anithab75524@gmail.com>
2021-04-21 14:02:15 +0530

    strings pgm on win env

---
+++ strings.py
@@ -0,0 +1,29 @@
+print('Brain\'s mother: He\'s not an angel. He\'s a very naughty boy!')
+print('one \nTwo \nThree')
+print("""this 
+is a 
+multiline 
+text""")
+print("Hi")
+print("""This
+is
+great""")
+print("spam" + 'eggs')
+print("2" + "2")
+print("spam" * 3)
+print(4 * '2')
+x = 2
+x += 3
+print(x)
+spam = "7"
+abc = 7
+print (abc)
+print(abc+7)
+x="this is a string"
+print(x + "!")
+x = input()
+print(x)
+name = input("Enter your name: ")
+print("Hello, " + name)
+age=int(input())
+print(age)
